e54d0d0cd85c7b07ef4bdb2e3900bf75424bb257
Alex Legkovsky <legkovsky.alexei@yandex.ru>
2025-06-17 22:36:38 +0300

    learned how to open files in tkinter

---
+++ 11_file.py
@@ -0,0 +1,28 @@
+from fileinput import filename
+from logging import LoggerAdapter
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+
+root = Tk()
+root.title('Learn to code at Codemy.com')
+
+
+# root.iconbitmap("some your favicon path")
+
+def open():
+    global my_image
+    root.filename = filedialog.askopenfilename(initialdir="path to file", title="Select a file",
+                                               filetypes=(("png files",
+                                                           "*.png"),
+                                                          ("all "
+                                                           "files",
+                                                           "*.*")))
+    my_label = Label(root, text=root.filename).pack()
+    my_image = ImageTk.PhotoImage(Image.open(root.filename))
+    my_image_label = Label(image=my_image).pack()
+
+
+my_button = Button(root, text="Open file", command=open).pack()
+
+mainloop()
